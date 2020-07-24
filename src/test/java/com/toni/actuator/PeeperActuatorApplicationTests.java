6896278bf5dadc943d43c7e5b25c8fb8543829e5
toni <ferreirocouto@gmail.com>
2020-07-24 11:44:15 +0100

    add actuator dependency

--- src/test/java/com/toni/actuator/PeeperActuatorApplicationTests.java
+++ src/test/java/com/toni/actuator/PeeperActuatorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.toni.thymeleaf;
+package com.toni.actuator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PeeperThymeleafApplicationTests {
+class PeeperActuatorApplicationTests {
 
 	@Test
 	void contextLoads() {
